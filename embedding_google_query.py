5112ad93dd48e569319bb1ac6988f9cf0bf7099c
Bhargavi <bhargaviburra2004@gmail.com>
2026-01-28 18:28:22 +0530

    Delete embedding_google_query.py

--- embedding_google_query.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
-
-embeddings = GoogleGenerativeAIEmbeddings(model = 'gemini-embedding-001')
-
-result = embeddings.embed_query('delhi is the captial of india')
-print(str(result))
